c3a9909b2ecd0f90f639d56d54fbefed9ed56727
LuisVad <20213tn002@utez.edu.mx>
2024-02-27 19:21:17 -0600

    [ADD] Avance del front

--- Cliente/src/main.js
+++ Cliente/src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+import '@asika32764/vue-animate/dist/vue-animate.css';
+
+Vue.use(VueAxios, axios);
+Vue.use(VueSweetalert2);
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
