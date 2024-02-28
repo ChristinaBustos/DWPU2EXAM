e1653144b1698ff2d53a182453149312b8db2ab8
Cristina <20213tn151@utez.edu.mx>
2024-02-27 22:09:25 -0600

    [UPDATE] read images

--- Cliente/src/modules/Books/Views/Books.vue
+++ Cliente/src/modules/Books/Views/Books.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <div>
-      <b-carousel style="text-shadow: 0px 0px 2px #000" indicators img-width="500" img-height="500">
-        <b-carousel-slide caption="First Slide" img-src="https://picsum.photos/1920/480/?image=10"></b-carousel-slide>
-        <b-carousel-slide caption="Second Slide" img-src="https://picsum.photos/1920/480/?image=12"></b-carousel-slide>
-        <b-carousel-slide caption="Third Slide" img-src="https://picsum.photos/1920/480/?image=22"></b-carousel-slide>
-        <b-carousel-slide caption="Fourth Slide" img-src="https://picsum.photos/1920/480/?image=23"></b-carousel-slide>
+      <b-carousel
+        style="text-shadow: 0px 0px 2px #000"
+        indicators
+        img-width="500"
+        img-height="500"
+      >
+        <b-carousel-slide
+           v-for="(book, index) in data" :key="index"
+          :caption="book.name"
+          :img-src="base64ToImage(book.cover)"
+        ></b-carousel-slide>
       </b-carousel>
     </div>
 
     <div>
-
       <div class="bodybuttons">
         <b-button v-b-modal.modal-save class="btnadd">
           <b-icon icon="plus"></b-icon> Registrar libro
@@ -18,21 +23,29 @@
       </div>
     </div>
 
-
     <div class="d-flex flex-wrap justify-content-around">
-    <b-col v-for="(book, index) in data" :key="index" class="d-flex d-fixed">
-      <b-card style="height: 100%; width:auto">
-        <b-card-title>{{ book.name }}</b-card-title>
-        <b-card-sub-title>{{ book.autor }}</b-card-sub-title>
-        <b-card-text>{{ book.publishDate }}</b-card-text>
-        <b-button @click="openUpdateModal(book)" variant="primary">Actualizar</b-button>
-        <b-button @click="deleteBook(book.id)" variant="danger">Eliminar</b-button>
-      </b-card>
-    </b-col>
+      <b-col v-for="(book, index) in data" :key="index" class="d-flex d-fixed">
+        <b-card style="height: 100%; width: auto">
+          <b-card-img :src="base64ToImage(book.cover)"></b-card-img>
+          <b-card-title>{{ book.name }}</b-card-title>
+          <b-card-sub-title>{{ book.autor }}</b-card-sub-title>
+          <b-card-text>{{ book.publishDate }}</b-card-text>
+          <b-button @click="openUpdateModal(book)" variant="primary"
+            >Actualizar</b-button
+          >
+          <b-button @click="deleteBook(book.id)" variant="danger"
+            >Eliminar</b-button
+          >
+        </b-card>
+      </b-col>
     </div>
 
     <ModalSave @book-updated="fetchData" />
-    <ModalUpdate ref="ModalUpdate" :book="selectedBook" @book-updated="fetchData" />
+    <ModalUpdate
+      ref="ModalUpdate"
+      :book="selectedBook"
+      @book-updated="fetchData"
+    />
   </div>
 </template>
 
@@ -56,19 +69,43 @@ export default {
       },
     };
   },
-  computed: {
-
-  },
+  computed: {},
   methods: {
     showModal() {
       this.show = true;
+    },
+    base64ToImage(base64String) {
+      // Extraer el tipo de la imagen desde la cadena Base64
+      const type = base64String.substring(
+        "data:image/".length,
+        base64String.indexOf(";base64")
+      );
+
+      // Crear un blob desde la cadena Base64
+      const byteCharacters = atob(base64String.split(",")[1]);
+      const byteArrays = [];
+      for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+        const slice = byteCharacters.slice(offset, offset + 512);
+        const byteNumbers = new Array(slice.length);
+        for (let i = 0; i < slice.length; i++) {
+          byteNumbers[i] = slice.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+      }
+      const blob = new Blob(byteArrays, { type: type });
+
+      // Crear una URL para la imagen
+      const url = URL.createObjectURL(blob);
+
+      // Retornar la URL de la imagen
+      return url;
     },
     fetchData() {
       axios
         .get("http://localhost:8080/api-book/")
         .then((response) => {
           this.data = response.data.data;
-          console.log(this.data);
         })
         .catch((error) => {
           console.error("Error al obtener datos de la API", error);
